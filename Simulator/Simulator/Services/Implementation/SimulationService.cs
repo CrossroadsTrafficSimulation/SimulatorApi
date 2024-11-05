91f1c05dfdc9b858cc58ee364fe54affe1be0cfb
KotVMeshke <dimon2003mail.ru37@gmail.com>
2024-11-05 14:40:45 +0300

    Syntax errors fix

--- Simulator/Simulator/Services/Implementation/SimulationService.cs
+++ Simulator/Simulator/Services/Implementation/SimulationService.cs
@@ -1,7 +1,4 @@
-﻿using DTO = Simulator.Model.RequestDto;
-using PROC = Simulator.Processes;
-
-using Simulator.Model.RequestDto;
+﻿using Simulator.Model.RequestDto;
 using Simulator.Model.ResponseDto;
 using Simulator.Processes;
 using Simulator.Services.Interface;
@@ -14,28 +11,28 @@ public class SimulationService(IMapper mapper) : ISimulationService
 {
     private List<Simulation> _simulations = null!;
     private const int SimulationTime = 24 * 60 * 60;
-    public void SetUpSimulations(List<DTO.Flow> flows,List<DTO.Edge> edges, List<DTO.Point> points, List<DTO.TrafficLight> trafficLights,int simulationsQuantity = 5)
-    {
-        var rand = new Random();
-        _simulations = new List<Simulation>(simulationsQuantity);
-        for (int i = 0; i < simulationsQuantity; i++)
-        {
-            _simulations.Add(new Simulation()
-            {
-                Edges = edges.Select(e => mapper.Map<PROC.Edge>(e)).ToList(),
-                Points = edges.Select(p => mapper.Map<PROC.Point>(p)).ToList(),
-                TrafficLights = edges.Select(tr => mapper.Map<PROC.TrafficLight>(tr)).ToList(),
-                Flows = edges.Select(f => mapper.Map<PROC.Flow>(f)).ToList()
-            });
-        }
-    }
+    //public void SetUpSimulations(List<> flows,List<DTO.Edge> edges, List<DTO.Point> points, List<DTO.TrafficLight> trafficLights,int simulationsQuantity = 5)
+    //{
+    //    var rand = new Random();
+    //    _simulations = new List<Simulation>(simulationsQuantity);
+    //    for (int i = 0; i < simulationsQuantity; i++)
+    //    {
+    //        _simulations.Add(new Simulation()
+    //        {
+    //            Edges = edges.Select(e => mapper.Map<PROC.Edge>(e)).ToList(),
+    //            Points = edges.Select(p => mapper.Map<PROC.Point>(p)).ToList(),
+    //            TrafficLights = edges.Select(tr => mapper.Map<PROC.TrafficLight>(tr)).ToList(),
+    //            Flows = edges.Select(f => mapper.Map<PROC.Flow>(f)).ToList()
+    //        });
+    //    }
+    //}
     private SimulationResult RunSimulation(Simulation simulation)
     {
-        for (int i = 0; i < SimulationTime; i++)
+        for (int currentTime = 0; currentTime < SimulationTime; currentTime++)
         {
-            simulation.ProcessTrafficLights();
-            simulation.PrecessPedestrians();
-            simulation.ProcessVehicles();
+            simulation.ProcessTrafficLights(currentTime);
+            simulation.PrecessPedestrians(currentTime);
+            simulation.ProcessVehicles(currentTime);
         }
         return null!;
     }
--- Simulator/Simulator/Utils/AutoMapperProfile.cs
+++ Simulator/Simulator/Utils/AutoMapperProfile.cs
@@ -1,6 +1,4 @@
-﻿using DTO = Simulator.Model.RequestDto;
-using PROC = Simulator.Processes;
-
+﻿
 using AutoMapper;
 
 namespace Simulator.Utils;
@@ -9,10 +7,7 @@ public class AutoMapperProfile : Profile
 {
     public AutoMapperProfile()
     {
-        CreateMap<DTO.Edge, PROC.Edge>();
-        CreateMap<DTO.Flow, PROC.Flow>();
-        CreateMap<DTO.Point, PROC.Point>();
-        CreateMap<DTO.TrafficLight, PROC.TrafficLight>();
+       
     }
 }
 
